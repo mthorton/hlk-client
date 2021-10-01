4299b03cfbb23aaae781a8bf7fba4d3e838018f6
Maxwell Horton <maxthomashorton@gmail.com>
2021-09-30 20:46:24 -0400

    error in environment file.

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'hook-line-and-keeper-app.heroku.com':
+    case 'hook-line-and-keeper-app.herokuapp.com':
         APIURL = 'https://hlk-server.herokuapp.com'
 }
 
